d16b933d9770b60f1166021513d9f952bd541241
lbarbier22 <barbier.lucas08@gmail.com>
2024-11-08 23:22:49 +0100

    chore : remove function that have been forgotten in resolve conflict

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -51,51 +51,6 @@ function Hello() {
   );
 }
 
-async function getBearer() {
-  const response = await fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-      'grant_type': 'client_credentials',
-      'client_id': '<DELETED>',
-      'client_secret': '<DELETED>'
-    })
-  });
-
-  if (response.ok) {
-    const data = await response.json();
-    console.log('Search results:', data.access_token);
-    return data.access_token; // Envoie le token via IPC
-  } else {
-    console.error('Search failed:', response.statusText);
-  }
-}
-
-async function searchAlbums(query, bearer) {
-  try {
-    const response = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=album&limit=3`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${bearer}`
-      }
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      console.log('Search results:', data);
-      return data;
-    } else {
-      console.error('Search failed:', response);
-      throw new Error(`Search failed with status: ${response.statusText}`);
-    }
-  } catch (error) {
-    console.error('Error during search:', error);
-    throw error;
-  }
-}
-
 export default function App() {
   return (
     <Router>
